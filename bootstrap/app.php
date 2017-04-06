fa35c625aa78128ed9d83f093a067ef5f3e47a2e
Rishabh Jain <jrishabh55@outlook.como>
2017-04-07 00:28:06 +0530

    Added Admin Login middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -66,6 +66,7 @@ $app->singleton(
  $app->routeMiddleware([
      'auth' => App\Http\Middleware\Authenticate::class,
      'api' => App\Http\Middleware\APIMiddleware::class,
+     'admin' => App\Http\Middleware\AdminMiddleware::class,
  ]);
 
 /*
