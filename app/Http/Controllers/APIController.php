2a6637903011e0a5db40b7e167107d0f9b09982d
Rishabh Jain <jrishabh55@outlook.como>
2017-04-07 00:25:54 +0530

    Added Extra APIs

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use function APIError;
+use App\Helpers\APIResponse;
 use App\Offer;
+use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use function integerValue;
+use function is_integer;
+use function is_numeric;
 use function response;
 
 class APIController extends Controller
@@ -15,19 +22,58 @@ class APIController extends Controller
      */
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('api');
+        $this->middleware('auth');
     }
 
-    public function getOffers()
+    public function getOffers(int $offer = NULL)
     {
-        return response()->json(Offer::active()->get()->toArray());
+        if(!empty($offer))
+            $json = Offer::active()->where('id',$offer)->first();
+        else
+            $json = Offer::active()->get()->toArray();
+
+        if(!empty($json))
+            return APIResponse('GetOffers',['offers' => $json]);
+        else
+            return APIError('GetOffers',['Entry not found' => 'The item you are trying to access cannot be found.'], 500);
+
     }
 
-    public function getUserData()
+    public function getUserData(int $user = NULL)
     {
-        if(Auth::check())
-            return response()->json(Auth::user());
-        else return response('Failed',401);
+        if(!empty($id) && is_integer($id))
+        {
+            $user = User::where('id',$id)->first();
+        }else if(Auth::check())
+        {
+            $user = Auth::user();
+        }else {
+            $user = NULL;
+        }
+        if(!empty($user))
+            return APIResponse('GetUserData',['user' => $user]);
+        else
+            return APIError('GetUserData',['Entry not found' => 'The item you are trying to access cannot be found.']);
+    }
+
+    public function getUserCredits(int $user = NULL)
+    {
+        if(!empty($user))
+        {
+            $credits = User::where('id',$user)->first()->credits ?? NULL;
+        }
+        else{
+            if(Auth::check())
+                $credits = Auth::user()->credits;
+            else
+                $credits = NULL;
+        }
+
+        if($credits)
+            return APIResponse('GetUsersCredits',['credits' => $credits]);
+        else
+            return APIError('GetUserCredits',['Entry not found' => 'The item you are trying to access cannot be found.']);
     }
 
 
